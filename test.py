ea3fe21da5cc2ba16a19ca62d8a11b35726150af
Andrew Jiang <andrew.jiang@bcgdv.com>
2016-07-14 17:30:16 -0700

    demo splitting works

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from src.splitterkit import readwave, writewave, split, combine
+from splitterkit import readwave, writewave, split, combine
 
 src = 'res/samples/f_disgust.wav'
 dest = 'res/output/somefile.wav'
@@ -11,3 +11,8 @@ print splitted[0][0][3], splitted[1][0][3]
 combined = combine(splitted + [data])
 print len(combined[1][0])
 writewave(dest, combined)
+
+for i in range(len(splitted)):
+    for j in range(len(splitted[i][1])):
+        destination = 'res/output/file-'+`i`+'-'+`j`+'.wav'
+        writewave(destination, (splitted[i][0],[splitted[i][1][j]]))
